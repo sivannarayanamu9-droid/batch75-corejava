0e09ed71721323f98abc2f2b8583b80412d981a8
sivannarayanamu9-droid <sivannarayana.mu9@gmail.com>
2026-07-14 08:09:40 +0530

    var generic datatype example committed

---
+++ JavaIntro/src/com/langaugefundamantals/TestVarDemo.java
@@ -0,0 +1,21 @@
+package com.langaugefundamantals;
+
+public class TestVarDemo {
+
+	void addition() {
+		var a = 10;// var is a generic datatype to store any value inside the methods or blocks
+		// var was inroduces in java 10 we can use var generic data type in methods or
+		// blocks
+		var b = 20;
+		var sum = a + b;
+		System.out.println("addition of two numbers are :" + sum);
+	}
+
+	public static void main(String[] args) {
+		System.out.println("main method started");
+		TestVarDemo objTestVarDemo = new TestVarDemo();
+		objTestVarDemo.addition();
+		System.out.println("main method ended");
+	}
+
+}
